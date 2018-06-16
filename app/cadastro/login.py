250c018a43b4f71f557bdeaac9411f000cebbeee
rodrigofarias08 <rodrigofarias08@gmail.com>
2018-06-15 22:14:15 -0300

    adicionado contribuição do Rodrigo

--- app/cadastro/login.py
+++ app/cadastro/login.py
@@ -1,1 +1,43 @@
+from app.painel.painel import *
+from app.cadastro.signup import *
+import json
+
+def login():
+    # Abre o arquivo no modo append, ou seja, cria o arquivo, caso não exista.
+    arquivo = open('cadastro.txt','a')
+    arquivo.close()
+    
+    arquivo = open('cadastro.txt','r') #modo read
+    #arquivo.tell()
+    #Retorna a posição atual do ponteiro de leitura. Cada caractere no arquivo 
+    #ocupa uma posição. Caracteres de quebra de linha são considerados também.
+    
+    #arquivo.seek(2)
+    #Define a posição atual do ponteiro de leitura. Neste caso está definindo 
+    #a posição 2.
+    
+    #arquivo.read() lê todo o arquivo a partir da posição do ponteiro e o 
+    #retorna em uma única string. A posição inicial do ponteiro é zero se não 
+    #foi alterada com seek(). Após executado o read() o ponteiro passa a ser a 
+    #última posição no arquivo.
+    
+    #arquivo.readlines() lê todo o arquivo e retorna cada linha como um item 
+    #de uma lista.
+    users = arquivo.readlines()
+    arquivo.close()
+    
+    if users == []:
+        print("Arquivo vazio.")
+        cadastrando_usuario()
+    else:
+        print("Arquivo não vazio.")
+    
+    arquivo = open('cadastro.txt','r')
+    users = arquivo.readlines()
+    arquivo.close()
+    
+    
+login()
+
+
 
--- app/cadastro/signup.py
+++ app/cadastro/signup.py
@@ -30,3 +30,42 @@ def recebe_endereco_usuario():
             print("Senha terá que ter letras e números")
         elif (endereco.replace(" ","").isalnum()):
             return endereco
+
+def recebe_senha_usuario():
+    while True:
+        senha = input("Cadastre uma senha: ")
+        aux = senha.find(" ")
+        if aux != -1:
+            print("A senha não pode conter espaço em branco.")
+        elif len(senha) <= 5:
+            print("A senha tem que possuir no mínimo 6 dígitos.")
+        elif senha.isalpha() or senha.isnumeric():
+            print("A senha terá que ter letras e números.")
+        elif senha.isalnum():
+            criptografia = "{}{}{}".format("dgSFHt4568gfhe", senha, "fdgfhQT12tyjhet")
+            break
+    
+    print("Cadastre agora uma pergunta secreta que só você sabe a resposta caso esqueça seu e-mail ou senha.")
+    print("Caso esqueça seu e-mail ou senha será feito esta pergunta e você terá de responder corretamente para recuperar o acesso.")
+    print("Cadastre a resposta com cuidado. Será considerado a igualdade de todos os caracteres para conferência.")
+    while True:
+        pergunta = input("Digite a pergunta secreta com no máximo 60 caracteres: ").strip()
+        if len(pergunta) <= 60:
+            break
+    while True:
+        resposta = input("Digite a resposta secreta com no máximo 20 caracteres: ").strip()
+        if len(resposta) <= 20:
+            criptografia2 = "{}{}{}".format("345ghghjGJGHK%$245", resposta, "dsfgSFGeretertwer3452435")
+            break
+    return [criptografia, criptografia2]
+
+# CADASTRANDO USUARIO
+def cadastrando_usuario():
+    nome_usuario = recebe_nome_usuario()
+    numero_usuario = recebe_numero_usuario()
+    endereco_usuario = recebe_endereco_usuario()
+    senha_usuario = recebe_senha_usuario()
+    print("Cadastro realizado com sucesso.")
+    arquivo = open('cadastro.txt','a')
+    arquivo.writelines([nome_usuario, senha_usuario, numero_usuario, endereco_usuario])
+    arquivo.close()
